d953343a1421a6eabc85ae8fa79cf103a278a8f9
Kelvis Borges <kelvis.borges@ms.senac.br>
2020-01-29 13:27:28 -0400

    intercepting http request with angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
+
+import { HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from './in-memory-database';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,7 +1,7 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { Category } from './categories/shared/category.model';
+import { Category } from './pages/categories/shared/category.model';
 
-export class InMemoryDb implements InMemoryDbService {
+export class InMemoryDatabase implements InMemoryDbService {
 
   // createDb( reqInfo?: import("angular-in-memory-web-api").RequestInfo): {} | import("rxjs").Observable<{}> | Promise<{}> {
   createDb() {
