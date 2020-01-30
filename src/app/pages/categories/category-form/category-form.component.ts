6c0f918ba1024f59390d05de300b0df624a78774
Kelvis Borges <kelvisbcc@gmail.com>
2020-01-30 01:46:01 -0300

    improving CategoryFormComponent

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { CategoryService } from './../shared/category.service';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { switchMap } from 'rxjs/operators';
+
+import toastr from 'toastr';
+import { Category } from '../shared/category.model';
 
 @Component({
   selector: 'app-category-form',
@@ -7,9 +15,129 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  currentAction: string; // new | edit
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submitingForm = false;
+  category = new Category();
+
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
   }
 
+  ngAfterContentChecked() {
+    this.setPageTitle();
+  }
+
+  // PRIVATE METHODS
+  private setCurrentAction() {
+    // tslint:disable-next-line: triple-equals
+    if (this.route.snapshot.url[0].path == 'new') {
+      this.currentAction = 'new';
+    } else {
+      this.currentAction = 'edit';
+    }
+
+    console.log(this.route.snapshot.url);
+
+  }
+
+  private buildCategoryForm() {
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null],
+    });
+  }
+
+  submitForm() {
+    this.submitingForm = true;
+
+    // tslint:disable-next-line: triple-equals
+    if (this.currentAction == 'new') {
+      this.createCategory();
+    } else { // edit
+      this.updateCategory();
+    }
+  }
+
+  private loadCategory() {
+    // tslint:disable-next-line: triple-equals
+    if (this.currentAction == 'edit') {
+      this.route.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(+params.get('id')))
+      )
+      .subscribe(
+        (category) => {
+          this.category = category;
+          this.categoryForm.patchValue(category); // binds loaded category data to CategoryForm
+        },
+        (error) => alert('Ocorreu um erro no servidor, tente mais tarde')
+      );
+    }
+  }
+
+  private setPageTitle() {
+    // tslint:disable-next-line: triple-equals
+    if (this.currentAction == 'new') {
+      this.pageTitle = 'Cadastro de Nova Categoria';
+    } else {
+      const categoryName = this.category.name || '';
+      this.pageTitle = 'Editando Categoria: ' + categoryName;
+    }
+  }
+
+  private createCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(category)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      );
+  }
+
+  private updateCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.updated(category)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      );
+  }
+
+  private actionsForSuccess(category: Category) {
+    toastr.success('Solicitação processada com sucesso!');
+
+    // redirect/reload component page
+    this.router.navigateByUrl('categories', { skipLocationChange: true})
+      .then(
+        () => this.router.navigate(['categories', category.id, 'edit'])
+      );
+  }
+
+  private actionsForError(error) {
+    toastr.error('Ocorreu um erro ao processar a sua solicitação!');
+
+    this.submitingForm = false;
+
+
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors; // ['nome ja existe', 'email em branco']
+    } else {
+      this.serverErrorMessages = ['Falha na comunicação com o servidor. Por favor, teste mais tarde.']
+    }
+
+  }
 }
