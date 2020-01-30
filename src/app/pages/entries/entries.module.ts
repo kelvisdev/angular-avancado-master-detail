e605174768148f191cd9d0f995263e692822f71f
Kelvis Borges <kelvis.borges@ms.senac.br>
2020-01-30 12:15:01 -0400

    adding entryFormComponent

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,15 +1,18 @@
+import { EntryFormComponent } from './entry-form/entry-form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from './entry-list/entry-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [EntryListComponent],
+  declarations: [EntryListComponent, EntryFormComponent],
   imports: [
     CommonModule,
-    EntriesRoutingModule
+    EntriesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class EntriesModule { }
---
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -0,0 +1,148 @@
+import { EntryService } from './../shared/entry.service';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { switchMap } from 'rxjs/operators';
+
+import toastr from 'toastr';
+import { Entry } from '../shared/entry.model';
+
+@Component({
+  selector: 'app-entry-form',
+  templateUrl: './entry-form.component.html',
+  styleUrls: ['./entry-form.component.css']
+})
+export class EntryFormComponent implements OnInit {
+
+  currentAction: string; // new | edit
+  entryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submitingForm = false;
+  entry = new Entry();
+
+  constructor(
+    private entryService: EntryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.setCurrentAction();
+    this.buildEntryForm();
+    this.loadEntry();
+  }
+
+  ngAfterContentChecked() {
+    this.setPageTitle();
+  }
+
+  // PRIVATE METHODS
+  private setCurrentAction() {
+    // tslint:disable-next-line: triple-equals
+    if (this.route.snapshot.url[0].path == 'new') {
+      this.currentAction = 'new';
+    } else {
+      this.currentAction = 'edit';
+    }
+
+    console.log(this.route.snapshot.url);
+
+  }
+
+  private buildEntryForm() {
+    this.entryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null],
+      type: [null, [Validators.required]],
+      amount: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      paid: [null, [Validators.required]],
+      categoryId: [null, [Validators.required]],
+    });
+  }
+
+  submitForm() {
+    this.submitingForm = true;
+
+    // tslint:disable-next-line: triple-equals
+    if (this.currentAction == 'new') {
+      this.createEntry();
+    } else { // edit
+      this.updateEntry();
+    }
+  }
+
+  private loadEntry() {
+    // tslint:disable-next-line: triple-equals
+    if (this.currentAction == 'edit') {
+      this.route.paramMap.pipe(
+        switchMap(params => this.entryService.getById(+params.get('id')))
+      )
+      .subscribe(
+        (entry) => {
+          this.entry = entry;
+          this.entryForm.patchValue(entry); // binds loaded entry data to EntryForm
+        },
+        (error) => alert('Ocorreu um erro no servidor, tente mais tarde')
+      );
+    }
+  }
+
+  private setPageTitle() {
+    // tslint:disable-next-line: triple-equals
+    if (this.currentAction == 'new') {
+      this.pageTitle = 'Cadastro de Novo Lançamento';
+    } else {
+      const entryName = this.entry.name || '';
+      this.pageTitle = 'Editando Lançamento: ' + entryName;
+    }
+  }
+
+  private createEntry() {
+    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+
+    this.entryService.create(entry)
+      .subscribe(
+        entry => this.actionsForSuccess(entry),
+        error => this.actionsForError(error)
+      );
+  }
+
+  private updateEntry() {
+    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+
+    this.entryService.updated(entry)
+      .subscribe(
+        entry => this.actionsForSuccess(entry),
+        error => this.actionsForError(error)
+      );
+  }
+
+  private actionsForSuccess(entry: Entry) {
+    toastr.success('Solicitação processada com sucesso!');
+
+    // redirect/reload component page
+    this.router.navigateByUrl('entries', { skipLocationChange: true})
+      .then(
+        () => this.router.navigate(['entries', entry.id, 'edit'])
+      );
+  }
+
+  private actionsForError(error) {
+    toastr.error('Ocorreu um erro ao processar a sua solicitação!');
+
+    this.submitingForm = false;
+
+
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors; // ['nome ja existe', 'email em branco']
+    } else {
+      this.serverErrorMessages = ['Falha na comunicação com o servidor. Por favor, tente mais tarde.']
+    }
+
+  }
+}
