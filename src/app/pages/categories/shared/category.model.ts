afcda0b0599fb8421a90e2df24372d4dda335473
Kelvis Borges <kelvisbcc@gmail.com>
2020-02-04 07:41:25 -0300

    improving BaseResourceService and refactoring CategoryService

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -9,7 +9,6 @@ export class Category extends BaseResourceModel {
     super();
   }
 
-
   static fromJson(jsonData: any): Category {
     return Object.assign(new Category(), jsonData);
   }
---
+++ src/app/shared/services/base-resource.service.ts
@@ -0,0 +1,77 @@
+import { BaseResourceModel } from '../models/base-resource.model';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap  } from 'rxjs/operators';
+import { Injectable, Injector } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core/testing';
+
+export abstract class BaseResourceService<T extends BaseResourceModel> {
+
+  protected http: HttpClient;
+
+  constructor(protected apiPath: string, protected injector: Injector) {
+    this.http = injector.get(HttpClient); // http pega uma instancia pronta
+  }
+
+  getAll(): Observable<T[]> {
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToResources)
+    );
+  }
+
+  getById(id: number): Observable<T> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToResource)
+    );
+  }
+
+  create(resource: T): Observable<T> {
+    return this.http.post(this.apiPath, resource).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToResource)
+    );
+  }
+
+  updated(resource: T): Observable<T> {
+    const url = `${this.apiPath}/${resource.id}`;
+
+    return this.http.put('sadad', resource).pipe(
+      catchError(this.handleError),
+      map(() => resource)
+    );
+  }
+
+  delete(id: number): Observable<any> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
+  // PROTECTED METHODS
+
+  protected jsonDataToResources(jsonData: any[]): T[] {
+    const resources: T[] = [];
+
+    jsonData.forEach(e => resources.push(e as T));
+
+    return resources;
+  }
+
+  protected jsonDataToResource(jsonData: any): T {
+    return jsonData as T;
+  }
+
+  protected handleError(error: any): Observable<any> {
+    console.log('erro na requisicao', error);
+
+    return throwError(error);
+  }
+
+}
